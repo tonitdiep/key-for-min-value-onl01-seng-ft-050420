e1d3c0d061da3811ca1f6eaf1a069bdc2271e469
Toni  Diep <adiep123@gmail.com>
2020-05-05 20:29:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   min_k = nil 
   min_v = nil
   name_hash.each do |k, v| 
-    if v < min_v && min_v = v
+    if v < min_v 
+      min_v = v
     else
     end
   end
